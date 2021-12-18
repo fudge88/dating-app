ef2b598ba416ff743e67f4088a8e5820b7b84f7c
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-18 13:51:55 +0000

    imported deendencies and connection added

---
+++ src/config/connection.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+const dbOptions = {
+  host: dbHost,
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+};
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+
+module.exports = sequelize;
