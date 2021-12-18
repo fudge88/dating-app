ca76534eb067cd1cd99e2a44fdfc972027c1bd98
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-18 13:12:35 +0000

    created connection and chema

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS dating_db;
+
+CREATE DATABASE dating_db;
+
+USE dating_db;
---
+++ src/config/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+
+const dbOptions = {
+  host: dbHost,
+  user: dbUser,
+  database: dbName,
+  password: dbPassword,
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+};
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+
+module.exports = sequelize;
