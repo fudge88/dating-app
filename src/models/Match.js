c0d701bdce7ef7ac28151f1d85ee2823f4491747
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-20 16:08:43 +0000

    user and match models craeted

--- src/models/Match.js
+++ src/models/Match.js
@@ -4,7 +4,40 @@ const connection = require("../config/connection");
 
 class Match extends Model {}
 
-const schema = {};
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  match_request_from: {
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+  match_request_to: {
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+  match_request_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  created_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  accepted_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+};
 
 const options = {
   sequelize: connection,
--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,42 @@ const connection = require("../config/connection");
 
 class User extends Model {}
 
-const schema = {};
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  user_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: { isNumeric: true },
+  },
+  user_gender: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  match_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "match",
+      key: "id",
+    },
+  },
+};
 
 const options = {
   sequelize: connection,
