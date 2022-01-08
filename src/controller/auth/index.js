f6b734128c0555dc7ed30987c8762dd5782f5888
SumaiaSorna <sorna.sumaia@gmail.com>
2022-01-08 13:21:59 +0000

    login successfully

--- src/controller/auth/index.js
+++ src/controller/auth/index.js
@@ -1,8 +1,44 @@
 const { getPayloadWithValidFieldsOnly } = require("../../helpers");
 const User = require("../../models/User");
 
-const login = (req, res) => {
-  res.render("login");
+const login = async (req, res) => {
+  try {
+    const payload = getPayloadWithValidFieldsOnly(
+      ["email", "password"],
+      req.body
+    );
+
+    if (Object.keys(payload).length !== 2) {
+      return res.status(400).json({
+        success: false,
+        error: "Please provide the valid fields in post body",
+      });
+    }
+
+    const user = await User.findOne({ where: { email: payload.email } });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User does not exist",
+      });
+    }
+
+    const validPassword = user.checkPassword(payload.password);
+
+    if (!validPassword) {
+      return res.status(401).json({
+        success: false,
+        error: "User not authorized",
+      });
+    }
+    return res.json({ success: true, data: "Login successful" });
+  } catch (error) {
+    console.log(`[ERROR]: Login user failed | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to login user" });
+  }
 };
 
 const signup = async (req, res) => {
